3fb2f1ad737affc8d31b62245328f269b23ac9e9
shengyun-zhou <GGGZ-1101-28@Live.cn>
2022-03-10 16:57:09 +0800

    Fix possible compile error

--- core/shared/platform/linux/platform_internal.h
+++ core/shared/platform/linux/platform_internal.h
@@ -61,6 +61,9 @@ typedef pthread_rwlock_t korp_rwlock;
 
 #define bh_socket_t int
 
+#define os_alloca alloca
+#define os_getpagesize getpagesize
+
 #if WASM_DISABLE_HW_BOUND_CHECK == 0
 #if defined(BUILD_TARGET_X86_64) || defined(BUILD_TARGET_AMD_64)            \
     || defined(BUILD_TARGET_AARCH64) || defined(BUILD_TARGET_RISCV64_LP64D) \
@@ -74,9 +77,6 @@ typedef jmp_buf korp_jmpbuf;
 
 #define os_setjmp setjmp
 #define os_longjmp longjmp
-#define os_alloca alloca
-
-#define os_getpagesize getpagesize
 
 typedef void (*os_signal_handler)(void *sig_addr);
 
